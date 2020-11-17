c447bf5e2e2ac8bbf3a3ea63fdaa0ba0a516d221
Adroit_Mkhari <adriot.mkhari@fnb.co.za>
2020-11-17 14:47:50 +0200

    Database Transactor Tests.

---
+++ src/test/java/selenium/web/driver/files/transform/DatabaseTransactorTests.java
@@ -0,0 +1,40 @@
+package selenium.web.driver.files.transform;
+
+import files.DatabaseTransactor;
+import org.testng.annotations.Test;
+
+import java.sql.ResultSet;
+
+public class DatabaseTransactorTests {
+    @Test
+    public void databaseTransactorDefaultConfigFile() {
+        try {
+            DatabaseTransactor databaseTransactor = new DatabaseTransactor();
+            databaseTransactor.connectToDatabase();
+            ResultSet resultSet = databaseTransactor.executeQuery("SELECT * FROM SOME_TABLE LIMIT 1");
+            while (resultSet.next()) {
+                String someValue = resultSet.getString("SOME_VALUE");
+                System.out.println("SOME_VALUE: " + someValue);
+            }
+            databaseTransactor.closeDatabaseConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void databaseTransactorSpecificConfigFile() {
+        try {
+            DatabaseTransactor databaseTransactor = new DatabaseTransactor("src\\test\\resources\\data_config.properties");
+            databaseTransactor.connectToDatabase();
+            ResultSet resultSet = databaseTransactor.executeQuery("SELECT * FROM SOME_TABLE LIMIT 1");
+            while (resultSet.next()) {
+                String someValue = resultSet.getString("SOME_VALUE");
+                System.out.println("SOME_VALUE: " + someValue);
+            }
+            databaseTransactor.closeDatabaseConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
