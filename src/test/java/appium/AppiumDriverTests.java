faff934d2d546b5c427abd27938a58dd3af6da87
Adroit_Mkhari <adriot.mkhari@fnb.co.za>
2020-11-17 23:01:24 +0200

    Appium Driver Manager Setup Test.

--- src/test/java/appium/AppiumDriverTests.java
+++ src/test/java/appium/AppiumDriverTests.java
@@ -1,5 +1,6 @@
 package appium;
 import io.appium.java_client.AppiumDriver;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 import selenium.web.driver.DriverManagerFactory;
 import selenium.web.driver.DriverType;
@@ -10,6 +11,7 @@ public class AppiumDriverTests {
     public void appiumDriverStartAppTest() {
         DriverManager driverManager = DriverManagerFactory.getDriverManager(DriverType.APPIUM);
         AppiumDriver driver = driverManager.getAppiumDriver();
+        Assert.assertNotNull(driver);
         driverManager.quitAppiumDriver();
         System.out.println("Application Started Successfully.");
     }
